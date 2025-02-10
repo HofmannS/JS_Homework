48274c756b05e97b442b2b9264247dddd7e132ed
admin <sergehofmann@icloud.com>
2025-02-10 19:16:43 +0100

    Переписал код с использованием объекта product

--- Task #4/js/script.js
+++ Task #4/js/script.js
@@ -1,42 +1,47 @@
-const imageArray = ["https://avatars.mds.yandex.net/i?id=bcf239099f66e95017c5ea42b42859668ccd1a267175e79e-3910357-images-thumbs&n=13", "https://avatars.mds.yandex.net/i?id=ebaa310a7e3414c1aba40ad298da969d3c867aa3-7749798-images-thumbs&n=13", "https://avatars.mds.yandex.net/i?id=18badbf2da5b6d495807098a5c739432d168fe70-10448002-images-thumbs&n=13", "https://yandex-images.clstorage.net/bM97Z3f74/ca6949vPQ/AqkpIiRJtaNc3qChLi5wCDi2BGkb1PjikWJX5bVS1UKYb5hqhfiU121TVbGw0d57erL0UPpIHbvzSLLCkUqPjUegcCMUJcyfdNqgrRoKHCwkmiRoesCIOxHCwhlEu1HiEZ5hQ92Nndbw4zT4VDCasjoPNPULPKyTKxgvDwxbasiywWH6fNG8o1jeKUVOltCcjL-0ZRON3C_1zm4ZJE_RIjUeVffErcUq2BLweADIGnp2V-DpP_yEk-OQH0cE_xq43u0N0oC1sKuIGjFd7qKUQLw3-NVbTVgnrQ56GQwL-VKFGr2vSBGZ0rzOdMAE8JbSolcogf-cEJvzZBeXuCcCNKZBMW6YqdTDQN411XIa7HiYe9TJ152M21VKas00H8n6nfYxL8B14QaAMjhQoJzOAhZ3JB3nAKSXX8gb8xAjylweSYmmOJHEI3xuMYXmEpywBFNskW-x1JPhVt4dcMuhHlm2UfvAfcGOQMoQaFg8Nro2s3AxZ5jEu09Av4tkR8qIpjEVPgS9QHs4HiW9eurQVBRj3F0LEfyHxf4eRaSjKSKJ-j2vaJEFOmiGfLiIbKaK9r8IBVe46BPXjI_3HB_K-E7FKXqUIazHqBLVcZJ62EyMs1y1N11Yc0nKThnoq9WKqQ4ZuxgdQRpsvlz4_BgGgkprpPFnOOCbowCnu5hzboSeGW3CLB1IS9gWZaX-GpDwjPfYTW-9EHtdii455EeJ-ik2gQ9IhU0-JMok8OQIcn5Kc4yVU7zoz_NYPz8An2b4PlWxtngx6NM04mHJWqLUwGAHoM0vvdj7uZLuUbwDJSbp8sEX_FVBjqSKjMB0vBYCmnMEGRscsONjYGvPED9C3FoVTTLEEegP9JpNDXp26FQkt_zZN4ngi1lW-t3slwn6KZLJf4ThaSJY_hDAIJBySn5zBHlz5AS3y4QTr3iLenSiOXViSDWkP2TGLfHubswcZMvwMadFgJv1CgaB4D-4"]
+let product = {
+    title: "Customized bike",
+    img: "https://avatars.mds.yandex.net/i?id=bcf239099f66e95017c5ea42b42859668ccd1a267175e79e-3910357-images-thumbs&n=13",
+    count: 10,
+    favorite: false
+}
 
 const btnFavorit = document.querySelector(".favorit")
 
-!!localStorage.getItem("isFavorit") ? btnFavorit.setAttribute("favorit", localStorage.getItem("isFavorit")) : btnFavorit.setAttribute("favorit", "off")
+let storedProduct = JSON.parse(localStorage.getItem("product")) || product;
+btnFavorit.setAttribute("favorit", storedProduct.favorite ? "on" : "off");
 
-btnFavorit.addEventListener("click", function(){
-    let fValue = btnFavorit.getAttribute("favorit")
-    if(fValue === "off"){
-        btnFavorit.setAttribute("favorit", "on") 
-        localStorage.setItem("isFavorit", "on")
-    }else{
-        btnFavorit.setAttribute("favorit", "off") 
-        localStorage.setItem("isFavorit", "off")
-    }    
-})
+btnFavorit.addEventListener("click", function () {
+    let jsonFavorit = JSON.parse(localStorage.getItem("product")) || product;
+    jsonFavorit.favorite = !jsonFavorit.favorite;
+    btnFavorit.setAttribute("favorit", jsonFavorit.favorite ? "on" : "off");
+    localStorage.setItem("product", JSON.stringify(jsonFavorit));
+});
+
 const btnMinus = document.querySelector(".minus")
 const btnPlus = document.querySelector(".plus")
 const quantityCounterElem = document.querySelector(".counter")
 
-let quantityCounterValue = !!localStorage.getItem("quantityCounter") ? +localStorage.getItem("quantityCounter") : 0
- 
-quantityCounterElem.innerText = quantityCounterValue
+let quantityCounterValue = storedProduct.count || 0
+ quantityCounterElem.innerText = quantityCounterValue
 
-btnMinus.addEventListener("click", function(){
-    quantityCounterValue--
-    if(quantityCounterValue < 0){quantityCounterValue = 0}
-    quantityCounterElem.innerText = quantityCounterValue
-    localStorage.setItem("quantityCounter", quantityCounterValue)
-})
-btnPlus.addEventListener("click", function(){
-    quantityCounterValue++
-    quantityCounterElem.innerText = quantityCounterValue
-    localStorage.setItem("quantityCounter", quantityCounterValue)
-})
+ btnMinus.addEventListener("click", function () {
+    JSON.parse(localStorage.getItem("product")) || product;
+    if (storedProduct.count > 0) {
+        storedProduct.count--;
+    }
+    quantityCounterElem.innerText = storedProduct.count;
+    localStorage.setItem("product", JSON.stringify(storedProduct));
+});
+btnPlus.addEventListener("click", function () {
+    JSON.parse(localStorage.getItem("product")) || product;
+    storedProduct.count++;
+    quantityCounterElem.innerText = storedProduct.count;
+    localStorage.setItem("product", JSON.stringify(storedProduct));
+});
 //-------------------------------------------------------------------------------
 // Решил добавить возможность добавлять карточки продукта, но вот как сделать независимые кнопки и счетчики не придумал
 const btnAddBikes = document.querySelector(".add-card")
-
+const imageArray = ["https://avatars.mds.yandex.net/i?id=bcf239099f66e95017c5ea42b42859668ccd1a267175e79e-3910357-images-thumbs&n=13", "https://avatars.mds.yandex.net/i?id=ebaa310a7e3414c1aba40ad298da969d3c867aa3-7749798-images-thumbs&n=13", "https://avatars.mds.yandex.net/i?id=18badbf2da5b6d495807098a5c739432d168fe70-10448002-images-thumbs&n=13", "https://yandex-images.clstorage.net/bM97Z3f74/ca6949vPQ/AqkpIiRJtaNc3qChLi5wCDi2BGkb1PjikWJX5bVS1UKYb5hqhfiU121TVbGw0d57erL0UPpIHbvzSLLCkUqPjUegcCMUJcyfdNqgrRoKHCwkmiRoesCIOxHCwhlEu1HiEZ5hQ92Nndbw4zT4VDCasjoPNPULPKyTKxgvDwxbasiywWH6fNG8o1jeKUVOltCcjL-0ZRON3C_1zm4ZJE_RIjUeVffErcUq2BLweADIGnp2V-DpP_yEk-OQH0cE_xq43u0N0oC1sKuIGjFd7qKUQLw3-NVbTVgnrQ56GQwL-VKFGr2vSBGZ0rzOdMAE8JbSolcogf-cEJvzZBeXuCcCNKZBMW6YqdTDQN411XIa7HiYe9TJ152M21VKas00H8n6nfYxL8B14QaAMjhQoJzOAhZ3JB3nAKSXX8gb8xAjylweSYmmOJHEI3xuMYXmEpywBFNskW-x1JPhVt4dcMuhHlm2UfvAfcGOQMoQaFg8Nro2s3AxZ5jEu09Av4tkR8qIpjEVPgS9QHs4HiW9eurQVBRj3F0LEfyHxf4eRaSjKSKJ-j2vaJEFOmiGfLiIbKaK9r8IBVe46BPXjI_3HB_K-E7FKXqUIazHqBLVcZJ62EyMs1y1N11Yc0nKThnoq9WKqQ4ZuxgdQRpsvlz4_BgGgkprpPFnOOCbowCnu5hzboSeGW3CLB1IS9gWZaX-GpDwjPfYTW-9EHtdii455EeJ-ik2gQ9IhU0-JMok8OQIcn5Kc4yVU7zoz_NYPz8An2b4PlWxtngx6NM04mHJWqLUwGAHoM0vvdj7uZLuUbwDJSbp8sEX_FVBjqSKjMB0vBYCmnMEGRscsONjYGvPED9C3FoVTTLEEegP9JpNDXp26FQkt_zZN4ngi1lW-t3slwn6KZLJf4ThaSJY_hDAIJBySn5zBHlz5AS3y4QTr3iLenSiOXViSDWkP2TGLfHubswcZMvwMadFgJv1CgaB4D-4"];
 function createCard(){
     let newCard = document.createElement("div")
     newCard.classList.add("product-card")
